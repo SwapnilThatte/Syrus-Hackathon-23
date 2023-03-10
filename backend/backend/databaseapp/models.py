be235b5faf89bd625e63ff2946c0d049228d17a4
Andyspiralio <anishlingam@gmail.com>
2023-03-10 11:36:12 +0530

    Added the workers model

--- backend/backend/databaseapp/models.py
+++ backend/backend/databaseapp/models.py
@@ -26,17 +26,48 @@ class Complaint(models.Model):
     Complaint_resolution_status = models.BooleanField(default=False)
 
 
-
 # Create your models here.
 class EmployerModel(models.Model):
     emp_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.IntegerField(max_length=8)
-    pfp = models.ImageField(null=False,blank=False)
+    pfp = models.ImageField(null=False, blank=False)
     aadharcard = models.ImageField(null=False, blank=False)
 
-
-
     def _str_(self):
-        return self.emp_id + "" + str(self.name)    
+        return self.emp_id + "" + str(self.name)
+
+
+work_type = (('plumber', 'PLUMBER'),
+             ('carpenter', 'CARPENTER'),
+             ('labour', 'LABOUR'),
+             )
+loc = (
+    ('mumbai', 'MUMBAI'),
+    ('thane', 'THANE'),
+    ('pune', 'PUNE'),
+)
+
+lang = (
+    ('english', 'ENGLISH'),
+    ('hindi', 'HINDI'),
+    ('marathi', 'MARATHI'),
+)
+
+
+class Workers(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=100)
+    email = models.EmailField()
+    photo = models.ImageField(null=False, blank=False)
+    phone = models.CharField(max_length=10)
+    Address = models.TextField()
+    Adhaarcard = models.ImageField(null=False, blank=False)
+    cityoforigin = models.CharField(max_length=20)
+    medicalhistory = models.TextField()
+    skills = models.CharField(max_length=100, choices=work_type, default=None)
+    prefferedworklocation = models.CharField(
+        max_length=100, choices=loc, default=None)
+    languages = models.CharField(max_length=100, choices=lang, default=None)
